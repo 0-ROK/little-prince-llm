23b29abe589eb2ac788d4fdbab9dfd6e142bf7ef
조영록 <0rok@kakao.com>
2024-07-10 23:18:32 +0900

    New : 어린왕자 RAG

--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,11 @@ import {
     Message,
 } from '@aws-sdk/client-bedrock-runtime';
 import dotenv from 'dotenv'
+import fs from 'fs'
+import pdf from 'pdf-parse'
+import { Pinecone } from '@pinecone-database/pinecone';
+import chunkText from './utils/chunkText';
+import OpenAi from 'openai'
 
 dotenv.config();
 
@@ -14,51 +19,146 @@ const port = 8080;
 
 app.use(bodyParser.json());
 
-console.log(process.env.AWS_REGION);
-
-const modelId = "anthropic.claude-3-sonnet-20240229-v1:0";
-
-const userMessage = "어린왕자에 대해 소개해주세요.";
-const conversation: Message[] = [
-    {
-        role: "user",
-        content: [{ text: userMessage }],
+if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
+    throw new Error("AWS credentials not found");
+}
+const inferenceModelId = "anthropic.claude-3-sonnet-20240229-v1:0";
+const llmClient = new BedrockRuntimeClient({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
-];
-
-const command = new ConverseCommand({
-    modelId,
-    messages: conversation,
-    inferenceConfig: { maxTokens: 512, temperature: 0.5, topP: 0.9 },
 });
 
-function main() {
 
-    if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-        throw new Error("AWS credentials not found");
-    }
+const embeddingModelId = 'text-embedding-ada-002';
+const embeddingClient = new OpenAi({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
-    const bedrockClient = new BedrockRuntimeClient({
-        region: process.env.AWS_REGION,
-        credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        },
-    });
+if (!process.env.PINECONE_API_KEY) {
+    throw new Error("Pinecone API key not found");
+}
+const pinecone = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY,
+});
+const vectorDbClient = pinecone.index('little-prince');
 
-    app.get('/generate', async (req, res) => {
+
+
+async function main() {
+    const text = await readPdf();
+    const chunkedText = chunkText(text, 350);
+
+    app.get('/generate', async (req: express.Request<{ userMessage: string }>, res: express.Response) => {
+
+        const { userMessage } = req.body;
+
+        const embeddingMessage = await embeddingText(userMessage ?? '');
+
+        const result = await vectorDbClient
+            .namespace('france')
+            .query({
+                topK: 3,
+                vector: embeddingMessage
+            });
+
+
+        console.log(result)
+
+
+        const conversation: Message[] = [
+            {
+                role: "user",
+                content: [{ text: userMessage }],
+            },
+            {
+                role: "assistant",
+                content: [
+                    { text: '다음에 제시되는 프랑스어 원문을 보고 사용자의 질문에 보다 풍성한 문학적 해설을 제공해주세요.' },
+                    { text: '<system>답변은 반드시 한국어로 하십시오. 답변에 프랑스어를 포함하지 마십시오.</system>' },
+                ]
+            },
+            {
+                role: "user",
+                content: result.matches.map((match) => {
+                    if (match.id === '') return { text: '' }
+                    if (isNaN(Number(match.id))) return { text: '' }
+                    return { text: chunkedText[Number(match.id)] }
+                }),
+            },
+        ];
+
+        const command = new ConverseCommand({
+            modelId: inferenceModelId,
+            messages: conversation,
+            inferenceConfig: { maxTokens: 4096, temperature: 0.5, topP: 0.9 },
+        });
+
         try {
-            const response = await bedrockClient.send(command);
-            res.json({ response: response });
+            const response = await llmClient.send(command);
+            res.json({
+                originalText: result.matches.map((match) => {
+                    if (match.id === '') return { text: '' }
+                    if (isNaN(Number(match.id))) return { text: '' }
+                    return { text: chunkedText[Number(match.id)] }
+                }),
+                response: response
+            });
         } catch (error) {
             res.status(500).json({ error });
         }
     });
 
+    app.get('/embedding', async (req, res) => {
+        const embeddingVectors = await Promise.all(chunkedText.map(async (chunk, index) => {
+            const embeddingVector = await embeddingText(chunk);
+            return embeddingVector;
+        }));
+
+        const result = await saveToVectorDB(embeddingVectors);
+        res.json({ result });
+    });
+
+    app.get('/how-many-vectors', async (req, res) => {
+        res.json({ result: chunkedText.length });
+    });
+
+
+    async function embeddingText(text: string) {
+        const response = await embeddingClient.embeddings.create({
+            model: embeddingModelId,
+            input: text,
+        })
+        return response.data[0].embedding;
+    }
+
+    async function saveToVectorDB(vectors: number[][]) {
+
+        const vectorsToInsert = vectors.map((vector, index) => ({
+            id: `${index}`,
+            values: vector,
+        }));
+
+        const result = await vectorDbClient.namespace('france').upsert(vectorsToInsert);
+
+        return result
+    }
+
+
     app.listen(port, () => {
         console.log(`Server running at http://localhost:${port}`);
     });
 }
+
+async function readPdf() {
+    const filePath = './docs/st_exupery_le_petit_prince.pdf';
+    const dataBuffer = fs.readFileSync(filePath);
+    const data = await pdf(dataBuffer);
+    return data.text;
+}
+
 
 main();
 
